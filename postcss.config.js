6ecf5e03179fe29141d4dac5bf4fa51f43dbce4d
payose <inipayose@gmail.com>
2021-10-18 13:11:29 +0100

    purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,29 @@
+const IN_PRODUCTION = process.env.NODE_ENV === "production";
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    // ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  }
-}
+  plugins: [
+    require ('tailwindcss'),
+    require ('autoprefixer'),
+    IN_PRODUCTION &&
+      require("@fullhuman/postcss-purgecss")({
+        content: [`./public/**/*.html`, `./src/**/*.vue`],
+        defaultExtractor(content) {
+          const contentWithoutStyleBlocks = content.replace(
+            /<style[^]+?<\/style>/gi,
+            ""
+          );
+          return (
+            contentWithoutStyleBlocks.match(
+              /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+            ) || []
+          );
+        },
+        safelist: [
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^(?!(|.*?:)cursor-move).+-move$/,
+          /^router-link(|-exact)-active$/,
+          /data-v-.*/,
+        ],
+      }),
+  ],
+};
