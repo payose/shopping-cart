b72c80ec49b8daa9147f0b01ce2e29bb58d350be
payose <inipayose@gmail.com>
2021-10-18 01:15:30 +0100

    home and product view styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    <div class="container">
-      <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-        <Cards
-        v-for="product in products" :key="product.id"
-        :product="product">
-        </Cards>
-      </div>
+  <div class="home w-screen">
+    <h2 class="py-6 text-center text-5xl font-bold text-green-700">Latest products</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mx-5 md:mx-10 md:p-10">
+      <Cards
+      v-for="product in products" :key="product.id"
+      :product="product"
+      class="bg-white shadow-sm rounded-sm">
+      </Cards>
     </div>
   </div>
 </template>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
-    <img :src="require(`../assets/images/${product.image}`)" alt="">
-    <h3 class="text-center">{{ product.name }}</h3>
-      <p class="description">{{product.description }}</p>
-      <h3 class="text-center"> ${{ product.price.toFixed(2) }}</h3>
+  <div class="flex items-center mx-5 md:mx-10">
+    <img :src="require(`../assets/images/${product.image}`)" alt="" >
+    <div class="md:ml-10 ">
+      <h3 class="">{{ product.name }}</h3>
+      <p class="leading-9">{{product.description }}</p>
+      <h3 class=""> <span>&#8358;</span> {{ product.price.toFixed(2) }}</h3>
      
      <!-- <div class="cart-total" v-if="productTotal">
         <h3>In Cart</h3>
         <h4> {{ productTotal }}</h4>
       </div> -->
 
-      <div class="button-container"> 
-        <button class="remove" @click="removeFromCart()">Remove</button>
-        <button class="add" @click="addToCart()">Add</button>
+      <div class="flex justify-between items-center"> 
+        <button class="hoverred-700 focus:bg-green-700 focus:outline-none shadow-sm bg-green-500 rounded-sm py-2 px-6" @click="addToCart()">Add</button>
+        <button class="hover:bg-red-700 focus:bg-red-700 focus:outline-none shadow-sm bg-red-500 rounded-sm py-2 px-6" @click="removeFromCart()">Remove</button>
       </div>
+    </div>
+    
   </div>
 </template>
 
